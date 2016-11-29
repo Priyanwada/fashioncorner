d9e766f0a20963f0fc84b979d67cdba2bbb68bfe
Priyanwada <priyanwada93@yahoo.com>
2016-11-29 18:58:26 +0530

    base files
    joomla file final set

---
+++ administrator/logs/com_virtuemart.log.php
@@ -0,0 +1,31 @@
+#
+#<?php die("Forbidden."); ?>
+
+2016-11-20 05:32:08 ERROR vmError: Warning, the Safe Path is empty, for safety reasons it is very important to create a folder in a path not accessible by an URL or unguessable name, create also a folder 'invoices' and 'keys' in it to store your sensitive data secure. Our suggested path is for your system 'C:\xampp\htdocs\vmfiles\'. <br>You can also use complex folder name as 'password' for example 'C:\xampp\htdocs\joomla/administrator/components/com_virtuemart\7UXdQPEKhmFp\'. Use this link to the <a href='http://localhost/joomla/administrator/index.php?option=com_virtuemart&view=config' >config</a>
+2016-11-20 05:33:41 ERROR vmError: Warning, the Safe Path is empty, for safety reasons it is very important to create a folder in a path not accessible by an URL or unguessable name, create also a folder 'invoices' and 'keys' in it to store your sensitive data secure. Our suggested path is for your system 'C:\xampp\htdocs\vmfiles\'. <br>You can also use complex folder name as 'password' for example 'C:\xampp\htdocs\joomla/administrator/components/com_virtuemart\ywmdr6VNXhQz\'. Use this link to the <a href='http://localhost/joomla/administrator/index.php?option=com_virtuemart&view=config' >config</a>
+2016-11-20 05:34:35 ERROR vmError: Warning, the Safe Path is empty, for safety reasons it is very important to create a folder in a path not accessible by an URL or unguessable name, create also a folder 'invoices' and 'keys' in it to store your sensitive data secure. Our suggested path is for your system 'C:\xampp\htdocs\vmfiles\'. <br>You can also use complex folder name as 'password' for example 'C:\xampp\htdocs\joomla/administrator/components/com_virtuemart\jvceyxDWY0qk\'. Use this link to the <a href='http://localhost/joomla/administrator/index.php?option=com_virtuemart&view=config' >config</a>
+2016-11-20 05:34:36 ERROR vmError: Warning, the Safe Path is empty, for safety reasons it is very important to create a folder in a path not accessible by an URL or unguessable name, create also a folder 'invoices' and 'keys' in it to store your sensitive data secure. Our suggested path is for your system 'C:\xampp\htdocs\vmfiles\'. <br>You can also use complex folder name as 'password' for example 'C:\xampp\htdocs\joomla/administrator/components/com_virtuemart\SI6LKbWE0p9Y\'. Use this link to the <a href='http://localhost/joomla/administrator/index.php?option=com_virtuemart&view=config' >config</a>
+2016-11-20 08:14:34 ERROR vmError: Warning, the Safe Path is empty, for safety reasons it is very important to create a folder in a path not accessible by an URL or unguessable name, create also a folder 'invoices' and 'keys' in it to store your sensitive data secure. Our suggested path is for your system 'C:\xampp\htdocs\vmfiles\'. <br>You can also use complex folder name as 'password' for example 'C:\xampp\htdocs\joomla/administrator/components/com_virtuemart\uM89gGpm3InK\'. Use this link to the <a href='http://localhost/joomla/administrator/index.php?option=com_virtuemart&view=config' >config</a>
+2016-11-20 09:03:11 ERROR vmError: Attention product is parent, please delete the children first
+2016-11-20 09:03:11 ERROR vmError: Attention product is parent, please delete the children first
+2016-11-20 09:03:11 ERROR vmError: Attention product is parent, please delete the children first
+2016-11-20 09:12:07 ERROR vmError: VmTableData Product Name in record is missing! Can't save the record with no Product Name.
+2016-11-20 09:12:07 ERROR vmError: You are not an administrator or the correct vendor, storing of product cancelled
+2016-11-20 09:15:57 ERROR vmError: Attention product is parent, please delete the children first
+2016-11-20 09:42:23 ERROR vmError: Attention product is parent, please delete the children first
+2016-11-20 09:42:41 ERROR vmError: Attention product is parent, please delete the children first
+2016-11-20 09:43:01 ERROR vmError: Attention product is parent, please delete the children first
+2016-11-20 09:47:50 ERROR vmError: VmTableData Product Name in record is missing! Can't save the record with no Product Name.
+2016-11-20 09:47:50 ERROR vmError: You are not an administrator or the correct vendor, storing of product cancelled
+2016-11-20 09:47:50 ERROR vmError: VmTableData Product Name in record is missing! Can't save the record with no Product Name.
+2016-11-20 09:47:50 ERROR vmError: You are not an administrator or the correct vendor, storing of product cancelled
+2016-11-20 09:47:50 ERROR vmError: VmTableData Product Name in record is missing! Can't save the record with no Product Name.
+2016-11-20 09:47:50 ERROR vmError: You are not an administrator or the correct vendor, storing of product cancelled
+2016-11-20 09:52:23 ERROR vmError: Attention product is parent, please delete the children first
+2016-11-20 09:52:40 ERROR vmError: Attention product is parent, please delete the children first
+2016-11-20 09:55:48 ERROR vmError: VmTableData Product Name in record is missing! Can't save the record with no Product Name.
+2016-11-20 09:55:48 ERROR vmError: You are not an administrator or the correct vendor, storing of product cancelled
+2016-11-20 09:55:48 ERROR vmError: VmTableData Product Name in record is missing! Can't save the record with no Product Name.
+2016-11-20 09:55:48 ERROR vmError: You are not an administrator or the correct vendor, storing of product cancelled
+2016-11-20 09:55:48 ERROR vmError: VmTableData Product Name in record is missing! Can't save the record with no Product Name.
+2016-11-20 09:55:48 ERROR vmError: You are not an administrator or the correct vendor, storing of product cancelled
---
+++ administrator/logs/error.php
@@ -0,0 +1,11 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-11-15 13:13:14 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-11-15T13:13:14+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-15T13:13:23+00:00	INFO ::1	joomlafailure	Empty password not allowed.
+2016-11-15T13:13:43+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-15T13:13:52+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2016-11-17T20:31:11+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
